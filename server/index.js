b6cd414c408541d8dd783eb2e1163d98403b394c
Celena Rock <celena.r.rock@gmail.com>
2021-11-17 15:59:53 -0600

    update

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use('/css', express.static(path.join(__dirname, 'client/style.css')))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/style.css'))
+})
 
 
 
